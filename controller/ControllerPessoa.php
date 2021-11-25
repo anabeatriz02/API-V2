ece735a4609af56972ddb7b42162388bbee819d2
ana bea <anabeatrizpbrito02@gmail.com>
2021-11-25 11:16:16 -0300

    save

--- controller/ControllerPessoa.php
+++ controller/ControllerPessoa.php
@@ -1,56 +1,59 @@
 <?php
 
-class ControllerPessoa{
+class ControllerPessoa
+{
 
     private $_method;
     private $_modelPessoa;
     private $_codPessoa;
 
-    public function __construct($model){
+    public function __construct($model)
+    {
 
         $this->_modelPessoa = $model;
         $this->_method = $_SERVER['REQUEST_METHOD'];
+        // echo $this->_method;exit;
 
-        //PERMITE RECEBER DADOS JSON ATRAVÉS DA REQUISIÇÃO
+        //PERMITE RECEBER DADOS JSON ATRÁVES DA REQUISIÇÃO
         $json = file_get_contents("php://input");
+        // echo $json;exit;
         $dadosPessoa = json_decode($json);
 
         $this->_codPessoa = $dadosPessoa->cod_pessoa ?? null;
-
     }
 
-    function router(){
-
+    function router()
+    {
+        // echo 'teste2';exit;
         switch ($this->_method) {
-            case 'GET':
 
+            case 'GET':
+                // echo 'teste';exit;
+                // echo $this->_codPessoa;exit;
                 if (isset($this->_codPessoa)) {
                     return $this->_modelPessoa->findById();
                 }
                 return $this->_modelPessoa->findAll();
-                
                 break;
 
             case 'POST':
+                // echo 'teste';exit;
                 return $this->_modelPessoa->create();
                 break;
-                    
+
             case 'PUT':
-                # code...
+                return $this->_modelPessoa->update();
                 break;
 
             case 'DELETE':
-                # code...
+                return $this->_modelPessoa->delete();
                 break;
-                    
+
             default:
                 # code...
                 break;
         }
-
     }
-
-
 }
 
 ?>
--- model/ModelPessoa.php
+++ model/ModelPessoa.php
@@ -16,7 +16,7 @@ class ModelPessoa{
         $json = file_get_contents("php://input");
         $dadosPessoa = json_decode($json);
 
-        //RECEBIMENTO DOS DADOS DO POSTMAN
+        // //RECEBIMENTO DOS DADOS DO POSTMAN
         $this->_codPessoa = $dadosPessoa->cod_pessoa ?? null;
         $this->_nome= $dadosPessoa->nome ?? null;
         $this->_sobrenome= $dadosPessoa->sobrenome ?? null; 
@@ -24,6 +24,16 @@ class ModelPessoa{
         $this->_celular= $dadosPessoa->celular ?? null;
         $this->_fotografia= $dadosPessoa->fotografia ?? null;
 
+        // $this->_nome = $_POST["nome"] ?? null;
+        // $this->_sobrenome = $_POST["sobrenome"] ?? null;
+        // $this->_email = $_POST["email"] ?? null;
+        // $this->_celular = $_POST["celular"] ?? null;
+        // $this->_fotografia = $_FILES["fotografia"]["name"] ?? null;
+
+        // echo '<pre>';
+        // var_dump($_FILES);
+        // echo '</pre>';exit;
+        
         $this->_conn = $conn;
 
     }
@@ -61,13 +71,18 @@ class ModelPessoa{
 
         $sql = "INSERT INTO tbl_pessoa (nome, sobrenome, email, celular, fotografia) VALUES (?, ?, ?, ?, ?)";
 
+        $extensao = pathinfo($this->_fotografia, PATHINFO_EXTENSION);
+        $novoNomeArquivo = md5(microtime()) . ".$extensao";
+
+        move_uploaded_file($_FILES["fotografia"]["tmp_name"], "../upload/$novoNomeArquivo");
+
         $stm = $this->_conn->prepare($sql);
 
         $stm->bindValue(1, $this->_nome);
         $stm->bindValue(2, $this->_sobrenome);
         $stm->bindValue(3, $this->_email);
         $stm->bindValue(4, $this->_celular);
-        $stm->bindValue(5, $this->_fotografia);
+        $stm->bindValue(5, $novoNomeArquivo);
 
         if ($stm->execute()) {
             return "Success";
@@ -75,9 +90,47 @@ class ModelPessoa{
             return "Error";
         }
 
+    }
 
+    public function delete(){
+
+        $sql = "DELETE FROM tbl_pessoa WHERE cod_pessoa = ?";
+
+        $stmt = $this->_conn->prepare($sql);
+
+        $stmt->bindValue(1, $this->_codPessoa);
+
+        if ($stmt->execute()) {
+            return "Dados excluídos com sucesso!";
+        }
 
     }
+
+    public function update(){
+
+        $sql = "UPDATE tbl_pessoa SET 
+        nome = ?,
+        sobrenome = ?,
+        email = ?,
+        celular = ?,
+        fotografia = ?
+        WHERE cod_pessoa = ?";
+
+        $stmt = $this->_conn->prepare($sql);
+
+        $stmt->bindValue(1, $this->_nome);
+        $stmt->bindValue(2, $this->_sobrenome);
+        $stmt->bindValue(3, $this->_email);
+        $stmt->bindValue(4, $this->_celular);
+        $stmt->bindValue(5, $this->_fotografia);
+        $stmt->bindValue(6, $this->_codPessoa);
+
+        if ($stmt->execute()) {
+            return "Dados alterados com sucesso!";
+        }
+
+    }
+
 
 }
 
